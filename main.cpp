839fb03fe56b1e68244b7c9dca39979b2d7f1751
Milutin Jovanović <mjp@voreni.com>
2022-04-03 21:50:40 -0400

    Hello, World! :-)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-int main()
-{
-   std::cout << "Hello World!\n";
+#include <lua.hpp>
+
+
+int main(int argc, char ** argv) {
+   lua_State *L = luaL_newstate();
+   luaL_openlibs(L);
+
+   // Our Lua code, it simply prints a Hello, World message
+   const char *code = "print('Hello, World!')";
+
+   // here we load the string and use lua_pcall for run the code
+   if (luaL_loadstring(L, code) == LUA_OK) {
+      if (lua_pcall(L, 0, 0, 0) == LUA_OK) {
+         // if it was executed successfuly we remove the code from the stack
+         lua_pop(L, lua_gettop(L));
+      }
+   }
+
+   lua_close(L);
    return 0;
 }
