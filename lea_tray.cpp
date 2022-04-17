f80c0259796ef393becc5218d22ac20a764c5ac8
Milutin Jovanović <mjp@voreni.com>
2022-04-17 00:46:58 -0400

    Now GPL licensed.

--- lea_tray.cpp
+++ lea_tray.cpp
@@ -1,3 +1,21 @@
+/*
+    Lea - Lightweight UI for your tools.
+    Copyright (C) 2022   Milutin Jovanović
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "lea_tray.hpp"
 #include "lea_util.hpp"
 
